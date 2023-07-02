c44edd0774131822e6610e4efd145d8df49eb305
Raghad <rghdhnya@gmail.com>
2023-07-02 15:32:34 +0300

    print student courses with their marks

--- fipr.py
+++ fipr.py
@@ -48,7 +48,8 @@ class Student:
     # method to get_student_courses
     def get_student_courses(self):
         # TODO 6 print student courses with their marks
-        pass
+        for i in self.courses_list:
+            print(f"Course : {i.course_name} , Mark : {i.course_mark}")
 
     # method to get student_average as a value
     def get_student_average(self):
